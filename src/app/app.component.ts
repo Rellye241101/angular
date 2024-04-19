22e070d786ee7ca2584e56d65fb8f785f7373345
Rellye241101 <bmsbrandon91@gmail.com>
2024-04-18 20:53:07 -0600

    Template listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router'
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, 
+  HeaderComponent, AboutComponent,TestimonialsComponent, 
+  GalleryComponent, NavComponent,  FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portafolios2';
+  title = 'website';
 }
